4c71557b4fbc90269553ba8b1e666c6ad6f9e970
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2025-02-02 03:02:30 +0700

    feat: client image route

---
+++ src/client/image/client.image.router.ts
@@ -0,0 +1,32 @@
+import express from "express"; 
+import type { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import multer from "multer";
+
+import * as ClientImageService from "./client.image.service";
+import { errorResponse, successResponse } from "../../../util/response/response.util";
+
+export const clientImageRouter = express.Router();
+
+const upload = multer({ storage: multer.memoryStorage() }); 
+
+clientImageRouter.post("/", 
+    upload.single("image"),
+    async(req: Request, res: Response, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            errorResponse(res, { message: errors.array()[0].msg }, 400);
+        }
+        try {
+            if (!req.file) {
+                errorResponse(res, { message: "Please provide an image to be uploaded" }, 400);
+            }
+
+            const image = req.file as Express.Multer.File;
+            const newImage = await ClientImageService.addClientImage(image);
+            successResponse(res, newImage, undefined, 201);
+        } catch(err: any) {
+            next(err);
+        }
+    }
+);
---
+++ src/client/image/client.image.service.ts
@@ -0,0 +1,18 @@
+import path from "path";
+import fs from "fs/promises";
+
+export const addClientImage = async (image: Express.Multer.File): Promise<String> => {
+    const uploadDir = path.join(process.cwd(), "/uploads");
+    await fs.mkdir(uploadDir, { recursive: true }); 
+
+    const timestamp = new Date().toString().replace(/[:.]/g, "-");
+    const ext = image.mimetype.split("/")[1];
+    const uniqueFilename = `${timestamp}-${image.filename}.${ext}`;
+    const filePath = path.join(uploadDir, uniqueFilename);
+
+    const arrayBuffer = image.buffer;
+    const buffer = Buffer.from(arrayBuffer);
+    await fs.writeFile(filePath, buffer); 
+
+    return uniqueFilename;
+}
